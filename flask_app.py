626fcc5aac2df7cda52615d938486f86004f7a6b
drPilman <doktorpilman@gmail.com>
2021-09-27 15:31:18 +0300

    Эстетические изменения, форматирование

--- flask_app.py
+++ flask_app.py
@@ -2,6 +2,7 @@ import vk_api
 from RASPIS import TimeChecker, savelast, GetRaspis
 from flask import Flask, Response, request
 import random
+
 app = Flask(__name__)
 
 vk_session = vk_api.VkApi(token='2a85a114c3af837c332b88f804290e76ddf4f326025a20e352d897d41a3fd5c3c2e47bb56a1df0e226f83')
@@ -12,8 +13,9 @@ def GetUsersId():
     user_id_items = vk.groups.getMembers(group_id='207336652')
     return user_id_items['items']
 
+
 def sender(id, text):
-    vk.messages.send(user_id=id, message=text, random_id= random.getrandbits(64))
+    vk.messages.send(user_id=id, message=text, random_id=random.getrandbits(64))
 
 
 ##Функции для переконвертирования ответа с расписанием в формат сообщения
@@ -24,6 +26,7 @@ def RaspisForDay(id):
         message += i + '\n'
     sender(id, message)
 
+
 helpDict = {
     0: 'Понедельник:\n',
     1: 'Вторник:\n',
@@ -31,6 +34,7 @@ helpDict = {
     3: 'Четверг:\n',
     4: 'Пятница:\n',
 }
+
 
 def RaspisForWeek(id):
     raspis = GetRaspis(1)
@@ -43,12 +47,14 @@ def RaspisForWeek(id):
         message += '\n'
     sender(id, message)
 
+
 def RaspisForWeekDay(id, weekDay):
     raspis = GetRaspis(1)
     message = ''
     for i in raspis[weekDay]:
         message += i + '\n'
     sender(id, message)
+
 
 @app.route('/')
 def Main():
@@ -60,17 +66,20 @@ def Main():
         return "Я все отправил"
     return "Нечего отправлять "
 
+
 @app.route('/reset')
 def Reset():
     savelast(t=False)
     return 'Очистил!'
 
-@app.route('/GetEvent', methods = ['POST'])
+
+@app.route('/GetEvent', methods=['POST'])
 def GetEvent():
     data = request.get_json()
 
     if data['type'] == 'group_join':
-        sender(data['object']['user_id'], 'Добро пожаловать!✌🏻\nЭтот бот будет напоминать тебе о паре за 15 минут.\nСоветуем не отключай уведомления.\nСписок команд:\n“р” - расписание на текущий день\n“в” - расписание на всю неделю,\n“пн” - “пт” - расписание на определенный день')
+        sender(data['object']['user_id'],
+               'Добро пожаловать!✌🏻\nЭтот бот будет напоминать тебе о паре за 15 минут.\nСоветуем не отключай уведомления.\nСписок команд:\n“р” - расписание на текущий день\n“в” - расписание на всю неделю,\n“пн” - “пт” - расписание на определенный день')
 
     if data['type'] == 'message_new':
         msg = data['object']['message']['text'].lower()
@@ -95,4 +104,3 @@ def GetEvent():
             sender(id, 'Ты не подписан! Я с такими не общаюсь.')
 
     return Response('ok'), 200
-
